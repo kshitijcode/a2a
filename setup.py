5f4618a17620fea8058ebe4d348f746a1294d7cb
Kshitij Sharma <ksharma@atlassian.com>
2020-08-04 13:08:00 +0530

    Updated readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='a2a',  # How you named your package folder (MyLib)
     packages=['a2a'],  # Chose the same as "name"
-    version='0.2',  # Start with a small number and increase it with every change you make
+    version='0.3',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Finds corresponding service offerings in Microsoft Azure and Amazon AWS .',
     # Give a short description about your library
@@ -16,13 +16,13 @@ setup(
         'scrapy',
         'tabulate',
     ],
+    scripts=['a2a/runner.py'],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',  # Define that your audience are developers
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',  # Again, pick a license
-        'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3',  # Specify which python versions that you want to support
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
